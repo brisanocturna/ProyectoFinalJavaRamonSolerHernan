cc5e5a193ef507d5ee5337b2a95159c9ab27aee2
Brisanocturna <ramon.soler.hernan@gmail.com>
2018-02-26 14:28:48 +0100

    Iniciansdo el proyecto scroll tamaño definido

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.WindowEvent;
+
+
 public class Controller {
+    static Double Height=0.0;
+
+    @FXML private AnchorPane listaNoticias;
+    @FXML private ScrollPane scrollNoticias;
+
+    public void cambiarScroll(){
+        scrollNoticias.setPrefHeight(Height);
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,36 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        primaryStage.setX(bounds.getMinX());
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setWidth(bounds.getWidth());
+        primaryStage.setHeight(bounds.getHeight());
+        Parent root =  FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root, bounds.getWidth(), bounds.getHeight());
+        primaryStage.setScene(scene);
+        Controller.Height=bounds.getHeight();
         primaryStage.show();
     }
 
